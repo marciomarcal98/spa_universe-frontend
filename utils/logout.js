fb02ef2958e8ec3d322d7cdde598f169b929d1a7
Marcio Marçal <marciomarcal.dev@gmail.com>
2024-11-10 21:31:01 -0300

    projeto finalizado

---
+++ utils/logout.js
@@ -0,0 +1,8 @@
+export function logout() {
+    const button = document.querySelector(".user button")
+    
+    button.addEventListener("click", () => {
+        localStorage.removeItem("token")
+        window.location.reload()
+    })
+}
